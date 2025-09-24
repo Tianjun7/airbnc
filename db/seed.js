b3a5223b93db981987c3e6cf3311d107088204a5
Tianjun7 <leovan754@gmail.com>
2025-09-24 11:49:43 +0100

    Syntax error video fixes

--- db/seed.js
+++ db/seed.js
@@ -1,28 +1,28 @@
 const db = require("./connection")
 const format = require("pg-format")
 
-async function seed(propertyTyes, properties){
+async function seed(propertyTypes, properties){
     await db.query(`DROP TABLE IF EXISTS properties;`)
-    await db.query(`DROP TABLE IF EXISTS property-types;`)
+    await db.query(`DROP TABLE IF EXISTS property_types;`)
 
-    await db.query(`CREATE TABLE property-types(
-        property_type VARCHAR(40) NOT NULL
+    await db.query(`CREATE TABLE property_types(
+        property_type VARCHAR(40) NOT NULL,
         description VARCHAR
         );`)
 
     await db.query(`CREATE TABLE properties(
-        name VARCHAR(40) NOT NULL
-        property_type VARCHAR(40) NOT NULL REFERENCES property-types(property_type)
-        location VARCHAR(40)
-        price_per_night FLOAT(2)
-        description VARCHAR(40)
-        host_name VARCHAR(40)
+        name VARCHAR(40) NOT NULL,
+        property_type VARCHAR(40) NOT NULL REFERENCES property_types(property_type),
+        location VARCHAR(40),
+        price_per_night FLOAT(2),
+        description VARCHAR(40),
+        host_name VARCHAR(40),
         amenities VARCHAR ARRAY
         );`)
 
     await db.query (
-        format(`INSERT INTO property-types(property-type, description) VALUES %L`,
-            propertyTyes.map(({property_types, description}) => [property_types, description])
+        format(`INSERT INTO property_types(property_type, description) VALUES %L`,
+            propertyTypes.map(({property_type, description}) => [property_type, description])
         )
     )
 }
