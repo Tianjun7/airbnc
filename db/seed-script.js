f8e0a3b01899d0323f8dee9ad553c45042a616a6
Tianjun7 <leovan754@gmail.com>
2025-09-24 22:31:51 +0100

    properties table (insert statement error commented out)

--- db/seed-script.js
+++ db/seed-script.js
@@ -1,6 +1,6 @@
 const seed = require("../db/seed");
-const {propertyTypes} = require("../db/data/test/property-types.json")
-const {properties} = require("../db/data/test/properties.json")
+const propertyTypes = require("../db/data/test/property-types.json")
+const properties = require("../db/data/test/properties.json")
 
 seed(propertyTypes, properties);
 
--- db/seed.js
+++ db/seed.js
@@ -6,7 +6,7 @@ async function seed(propertyTypes, properties){
     await db.query(`DROP TABLE IF EXISTS property_types;`)
 
     await db.query(`CREATE TABLE property_types(
-        property_type VARCHAR(40) NOT NULL,
+        property_type VARCHAR(40) NOT NULL PRIMARY KEY,
         description VARCHAR
         );`)
 
@@ -14,7 +14,7 @@ async function seed(propertyTypes, properties){
         name VARCHAR(40) NOT NULL,
         property_type VARCHAR(40) NOT NULL REFERENCES property_types(property_type),
         location VARCHAR(40),
-        price_per_night FLOAT(2),
+        price_per_night INTEGER,
         description VARCHAR(40),
         host_name VARCHAR(40),
         amenities VARCHAR ARRAY
@@ -25,6 +25,13 @@ async function seed(propertyTypes, properties){
             propertyTypes.map(({property_type, description}) => [property_type, description])
         )
     )
+
+    // await db.query (
+    //     format(`INSERT INTO properties(name, property_type, location, price_per_night, description, host_name, amenities) VALUES %L`,
+    //         properties.map(({name, property_type, location, price_per_night, description, host_name, amenities}) => 
+    //             [name, property_type, location, price_per_night, description, host_name, amenities])
+    //     )
+    // )
 }
 
 module.exports = seed;
