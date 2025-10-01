5c13b3940790b1f8a72c39459829099d2ba4a072
Tianjun7 <leovan754@gmail.com>
2025-10-01 22:36:08 +0100

    GET /api/properties

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const db = require("./db/connection")
+const { getProperties } = require("./controllers/properties")
+
 const app = express()
 
+app.get("/api/properties", getProperties)
 
 module.exports = app
